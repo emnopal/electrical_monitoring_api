8a3ce1aa24b28a789bc40c3a09c6d157d90ab01c
Muhammad Naufal <mnaufal0999@gmail.com>
2023-01-03 11:31:24 +0700

    i forgot that i use free tier of firebase :p

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import {config} from './src/configs/server.js';
 import router from './src/routes/electricalMonitoringRoute.js';
-import * as functions from 'firebase-functions';
 
-const app = config(express(), router);
+export const app = config(express(), router);
 
-// use this for local development
-// app.set('port', 2023);
+app.set('port', 2023);
 
-// app.listen(app.get('port'), () => {
-//     console.log(`server is running on: http://localhost:${app.get('port')}`);
-// });
-
-exports.app = functions.https.onRequest(app);
+app.listen(app.get('port'), () => {
+    console.log(`server is running on: http://localhost:${app.get('port')}`);
+});
