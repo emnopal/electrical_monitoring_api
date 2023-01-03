fda7a223cb4bf43c5fa6901d4f516f1bee0ed6cb
Muhammad Naufal <mnaufal0999@gmail.com>
2023-01-03 11:20:39 +0700

    config some code to follow firebase standard

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import {config} from './src/configs/server.js';
 import router from './src/routes/electricalMonitoringRoute.js';
+import * as functions from 'firebase-functions';
 
 const app = config(express(), router);
-app.set('port', 2023);
 
-app.listen(app.get('port'), () => {
-    console.log(`server is running on: http://localhost:${app.get('port')}`);
-});
+// use this for local development
+// app.set('port', 2023);
+
+// app.listen(app.get('port'), () => {
+//     console.log(`server is running on: http://localhost:${app.get('port')}`);
+// });
+
+exports.app = functions.https.onRequest(app);
